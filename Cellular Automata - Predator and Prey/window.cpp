173e42c5b3ddd667f7e4f5350a56afecafd3d711
Christpher Aucoin <criscross03@gmail.com>
2017-11-06 18:37:56 -0600

    More experimenting with window class:

--- Cellular Automata - Predator and Prey/window.cpp
+++ Cellular Automata - Predator and Prey/window.cpp
@@ -99,29 +99,23 @@ Window::Window(int width, int height, const char* title)
 	glfwSetWindowShouldClose(window, false);
 
 	this->shaderProgram();
-	
-	float vertices[] = {
-		-0.5f,-0.5f,0.0f, //Bottom Left
-		 0.5f,-0.5f,0.0f, //Bottom Right
-		-0.5f,0.5f,0.0f,  //Top Left
-		 0.5f, 0.5f, -0.0f//Top Right
-	};
 
-	int indices[] = {
-		0,1,3,
-		0,2,3
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f
 	};
 
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -147,9 +141,9 @@ void Window::renderMain()
 	glUseProgram(shaderPrgrm);
 	glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 	glBindVertexArray(VAO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
-	glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, 0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+	//glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, 0);
 
 	glfwSwapBuffers(window);
 	glfwPollEvents();
